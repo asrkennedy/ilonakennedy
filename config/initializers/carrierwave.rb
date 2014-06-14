269f8744b8afdc375802f3bd8c2c1cbc48a9b976
Andrea Kennedy <asrkennedy@gmail.com>
2014-06-14 16:05:28 +0100

    carrierwave initialiser

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    # Configuration for Amazon S3 should be made available through an Environment variable.
+    # For local installations, export the env variable through the shell OR
+    # if using Passenger, set an Apache environment variable.
+    #
+    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+    #
+    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+
+    # Configuration for Amazon S3
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+  config.fog_directory = 'ilonakennedy'
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
+  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+end
