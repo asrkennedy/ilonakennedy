d84c5c52c7b23ed1fd7879a061d63520cc2445db
Andrea Kennedy <asrkennedy@gmail.com>
2014-06-15 23:10:42 +0100

    adds works validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,7 @@ class Work < ActiveRecord::Base
   belongs_to :category
   belongs_to :collection
   attr_accessible :category_id, :date, :name, :price, :collection_id, :works_image, :works_image_cache, :remove_works_image
+  validates_presence_of :name, :price, :collection_id, :works_image, :category_id
 
   mount_uploader :works_image, WorksImageUploader
 
