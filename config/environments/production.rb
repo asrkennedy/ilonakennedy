b752c2306589c90c535a0dd1a8129b2a9553d587
Andrea Kennedy <asrkennedy@gmail.com>
2014-06-18 13:44:50 +0100

    modifies production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ IlonaKennedy::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js', 'rich/editor.css', 'rich/base.js']
+  # config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js', 'rich/editor.css', 'rich/base.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
